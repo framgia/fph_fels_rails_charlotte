f19b56b35a88c46ecb90317560742efa1feacdee
Charlotte <charlotte.sacmar@gmail.com>
2018-12-07 11:04:15 +0800

    Updated spacing and Fixed log-in access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < ActionController::Base
   private
     def logged_in_user
       unless logged_in?
-      	flash[:danger] = "Please log in."
-      	redirect_to login_url
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def logged_out_user
+      if logged_in?
+        flash[:danger] = "You are still logged in."
+        redirect_to root_url
       end
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_out_user, only: [:new, :create]
+
   def new
   end
 
