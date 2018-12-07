43fa44323f053653c363ea5fc52f8452616fc922
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-07 11:06:38 +0800

    Develop Log-in with Authentication (#5)
    * Develop Log-in with Authentication
    
    * Updated Application Controller, Stylesheet and Users Controller
    
    * Updated spacing and Fixed log-in access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def logged_out_user
+      if logged_in?
+        flash[:danger] = "You are still logged in."
+        redirect_to root_url
+      end
+    end
 end
