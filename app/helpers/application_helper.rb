34fe2631de2c2c3ed377ecd2da828791f7593056
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-10 12:56:49 +0800

    Create Users List Page (#8)
    * Create Users List Page
    
    * Updated List with Default Image and Seeders
    
    * Fixed indentation and spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def get_img(user)
+    if user.avatar?
+      image_tag user.avatar.url 
+    else
+      image_tag 'https://i.imgur.com/z51mKaZ.png'
+    end
+  end
 end
