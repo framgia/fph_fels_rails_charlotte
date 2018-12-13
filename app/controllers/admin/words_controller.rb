999d669989a34c021f45f00611449d58453c2577
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-13 14:00:47 +0800

    Can Edit Words and Choices (#13)
    * Can Edit Words and Choices
    
    * Keeps checkmark on correct choice during edit

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -23,8 +23,24 @@ class Admin::WordsController < ApplicationController
     end
   end
 
+  def edit
+    @word = Word.find(params[:id])
+    @category = Category.find_by_id(params[:category_id])
+  end
+
+  def update
+    @word = Word.find(params[:id])
+    @category = Category.find_by_id(params[:category_id])
+    if @word.update_attributes(word_params)
+      flash[:success] = "Word has been updated"
+      redirect_to admin_category_word_url
+    else
+      render 'edit'
+    end
+  end
+
   private
     def word_params
-      params.require(:word).permit(:content, choices_attributes: [ :content, :correct ])
+      params.require(:word).permit(:content, choices_attributes: [:id, :content, :correct ])
     end
 end
