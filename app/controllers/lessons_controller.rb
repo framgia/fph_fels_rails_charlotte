93de8f127a2e927c762ca3f87ed9a712fd1e7c85
Charlotte <charlotte.sacmar@gmail.com>
2018-12-17 17:16:19 +0800

    Results Page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,17 +1,24 @@
 class LessonsController < ApplicationController
   def create
     @category = Category.find_by_id(params[:category_id])
-    @lesson = Lesson.new(category_id: @category.id, user_id: current_user.id) #check how to properly pass parameters for this
+    @lesson = Lesson.new(category_id: @category.id, user_id: current_user.id)
 
-    if @lesson.save
+    if @lesson.save && @lesson.category.words.any?
       redirect_to new_lesson_answer_url(@lesson)
     else
-      flash[:danger] = "Lesson did not save"
+      flash[:danger] = "There are no words in this category yet"
       redirect_to categories_url
     end
   end
 
-  def update_result
-    @lesson.increment!(params[:result])
+  def show
+    @lesson = Lesson.find_by_id(params[:id])
+    @result = 0
+
+    @lesson.answers.each do |answer|
+      if answer.choice.correct? 
+        @result += 1
+      end
+    end
   end
 end
