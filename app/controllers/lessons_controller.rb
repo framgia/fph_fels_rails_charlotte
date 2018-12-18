65037e3361f7b9ad1e7d06833c9c8f69698a4805
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-18 16:20:00 +0800

    Lessons Learned (#18)
    * Lessons Learned
    
    * Updated categories index and words learned

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,14 @@
 class LessonsController < ApplicationController
+  def index
+    @lessons = current_user.lessons.paginate(page: params[:page], per_page: 3)
+  end
+
   def create
     @category = Category.find_by_id(params[:category_id])
     @lesson = Lesson.new(category_id: @category.id, user_id: current_user.id)
 
-    if @lesson.save && @lesson.category.words.any?
+    if @lesson.category.words.any?
+      @lesson.save
       redirect_to new_lesson_answer_url(@lesson)
     else
       flash[:danger] = "There are no words in this category yet"
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { maximum: 50 }
   has_many :words, dependent: :destroy
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
 end
