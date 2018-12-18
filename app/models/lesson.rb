36f6aed413380b1ab1cb3240517f09dfc9cef729
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-18 09:55:16 +0800

    Results Page (#17)
    * Results Page
    
    * Improved query
    
    * Fixed icon and created model method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :answers
+
+  def correct_answers
+    self.answers.joins(:choice).where(choices: {correct: true})
+  end
 end
