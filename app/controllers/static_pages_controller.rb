6a2a963e2e74db8f6d3a1e706256c3dabdb3807c
Charlotte <charlotte.sacmar@gmail.com>
2018-12-27 18:30:06 +0800

    Updated activity log

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @activities = Activity.paginate(page: params[:page]).order("created_at DESC")
+      @activities = Activity.where(user: current_user.following).or(Activity.where(user: current_user)).paginate(page: params[:page]).order("created_at DESC")
     end
   end
 end
