6e1708d8e8d44739a01a2a4f76bca787cb6b68a6
Charlotte <charlotte.sacmar@gmail.com>
2018-12-28 13:32:24 +0800

    Added restart or continue button for unfinished lesson

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @activities = Activity.paginate(page: params[:page]).order("created_at DESC")
-  	end
+    end
   end
 end
