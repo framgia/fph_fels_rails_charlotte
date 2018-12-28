6b2fc2f71f116d26aace4d3183af5662b949b62f
CharlotteDS <charlotte.sacmar@gmail.com>
2018-12-28 14:48:13 +0800

    Activity Log Update (#23)
    * Updated activity log
    
    * Feed updated with followed users' posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @activities = Activity.paginate(page: params[:page]).order("created_at DESC")
+      @activities = Activity.where("user_id IN (?) OR user_id = ?", current_user.following_ids, current_user.id).paginate(page: params[:page]).order("created_at DESC")
     end
   end
 end
