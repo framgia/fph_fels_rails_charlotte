c0307c790830276ecd21688285ec7113c02d1eec
Charlotte <charlotte.sacmar@gmail.com>
2019-01-04 19:02:45 +0800

    Fixed filter for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,9 @@ class CategoriesController < ApplicationController
   before_action :logged_in_user
   def index
     if params[:filter].eql?("learned")
-      @categories = Category.joins(:lessons).where.not(lessons: {result: nil})
+      @categories = current_user.categories.where.not(lessons: {result: nil})
     elsif params[:filter].eql?("unlearned")
-      @categories = Category.includes(:lessons).where(lessons: {result: nil})
+      @categories = Category.left_outer_joins(:lessons).where.not(id: current_user.categories.where.not(lessons: {result: nil})).group(:id)
     else
       @categories = Category.all
     end
