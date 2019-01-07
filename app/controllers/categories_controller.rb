8795ec56d2543e980878671108930f28ea9e533c
Charlotte <charlotte.sacmar@gmail.com>
2019-01-07 10:49:59 +0800

    Added active class for categories filter and created empty results page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,17 @@
 class CategoriesController < ApplicationController
   before_action :logged_in_user
   def index
+    @page = params[:filter]
     if params[:filter].eql?("learned")
       @categories = current_user.categories.where.not(lessons: {result: nil})
     elsif params[:filter].eql?("unlearned")
-      @categories = Category.left_outer_joins(:lessons).where.not(id: current_user.categories.where.not(lessons: {result: nil})).group(:id)
+      @categories = Category.left_outer_joins(:lessons)
+                            .where.not(id: current_user.categories
+                            .where.not(lessons: {result: nil}))
+                            .group(:id)
     else
       @categories = Category.all
+      @page = "all"
     end
     @categories = @categories.paginate(page: params[:page])
   end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def current_page?(state)
+    if @page.eql?(state)
+      "btn btn-default active"
+    else
+      "btn btn-default"
+    end
+  end
 end
