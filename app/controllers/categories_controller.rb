2c18cf36dc5f0ea4c95ccc9a984c556f7e39c5f0
CharlotteDS <charlotte.sacmar@gmail.com>
2019-01-08 10:03:27 +0800

    Categories Filter (#25)
    * Added Categories Filter
    
    * Fixed filter for categories
    
    * Added active class for categories filter and created empty results page
    
    * Refactored categories filter code and updated feedback message implementation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController
   before_action :logged_in_user
   def index
-    @categories = Category.paginate(page: params[:page])
+    @page = params[:filter]
+    if params[:filter].eql?("learned")
+      @categories = current_user.categories.where.not(lessons: {result: nil})
+    elsif params[:filter].eql?("unlearned")
+      @categories = Category.left_outer_joins(:lessons)
+                            .where.not(id: current_user.categories
+                            .where.not(lessons: {result: nil}))
+                            .group(:id)
+    else
+      @categories = Category.all
+      @page = "all"
+    end
+    @categories = @categories.paginate(page: params[:page])
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,5 @@
 module CategoriesHelper
+  def current_page?(state)
+    return "active" if @page.eql?(state)
+  end
 end
